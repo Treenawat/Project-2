85cfe6c80dfaa79d23c5cc03564f50fb626a108e
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-07 13:43:04 -0600

    Finish commented in abstract

--- Projecct2/src/MesoAbstract.java
+++ Projecct2/src/MesoAbstract.java
@@ -2,11 +2,14 @@ import java.util.Arrays;
 
 public abstract class MesoAbstract 
 {
+	//Protected String
 	protected String StID;
 	
+	//Abstract
 	public MesoAbstract(MesoStation mesoStation) {
 		StID = mesoStation.getStID();
 	}
 	
+	//Array created for MesoInherit
 	int[] average = new int[3];
 }
